62cc092abefae13fae3c080246e9c46cc61a2880
GKmargo <giovanitccamargo@gmail.com>
2024-04-23 16:59:54 -0300

    Exercicio 09

---
+++ Exercicios/src/br/edu/up/Exercicio09.java
@@ -0,0 +1,18 @@
+package br.edu.up;
+
+import br.edu.up.modelos.Numero;
+
+public class Exercicio09 {
+    public static void executar(){
+        Numero numeros = new Numero();
+        
+        for (int i = 0; i < 80; i ++){
+            numeros.getNum()[i] = Prompt.lerDecimal("Digite o número " + (i+1) + ":");
+            if(numeros.getNum()[i]>=10 && numeros.getNum()[i] <=150){
+                numeros.setContador(numeros.getContador()+1);
+            }
+        }
+
+        Prompt.imprimir("Quantidade de números no intervalo de 10 e 150: " + numeros.getContador());
+    }
+}
---
+++ Exercicios/src/br/edu/up/modelos/Numero.java
@@ -0,0 +1,20 @@
+package br.edu.up.modelos;
+
+public class Numero {
+    private double[] num = new double[80];
+    private int contador = 0;
+    
+    public double[] getNum() {
+        return num;
+    }
+    public void setNum(double[] num) {
+        this.num = num;
+    }
+    public int getContador() {
+        return contador;
+    }
+    public void setContador(int contador) {
+        this.contador = contador;
+    }
+    
+}
