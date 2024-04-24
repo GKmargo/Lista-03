31dd9dc3657cda0eaec574c1a0b379154d7a12e0
GKmargo <giovanitccamargo@gmail.com>
2024-04-24 09:45:27 -0300

    Exercicio 11

---
+++ Exercicios/src/br/edu/up/Exercicios/Exercicio11.java
@@ -0,0 +1,9 @@
+package br.edu.up.Exercicios;
+
+import br.edu.up.Exercicios.Modelos.Sexo;
+
+public class Exercicio11 {
+    public static void executar(){
+        Sexo pessoa = new Sexo();
+    }
+}
