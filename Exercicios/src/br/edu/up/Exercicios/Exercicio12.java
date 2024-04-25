288a198850fed5e53aa7f2e734fdc96fc439502b
Pedro Henrique Boldori Bier <124646793+Pedrohbbier@users.noreply.github.com>
2024-04-25 09:20:35 -0300

    Terminando ex12

--- Exercicios/src/br/edu/up/Exercicios/Exercicio12.java
+++ Exercicios/src/br/edu/up/Exercicios/Exercicio12.java
@@ -1,5 +1,35 @@
 package br.edu.up.Exercicios;
 
+import br.edu.up.Exercicios.Modelos.ConcessionariaCarangoVelho;
+
+
 public class Exercicio12 {
+
+    public static void executar(){
+
+        boolean resposta = true;
+        int ate2000 = 0;
+        int total = 0;
+
+        while (resposta) {
+            
+        ConcessionariaCarangoVelho concessionaria = new ConcessionariaCarangoVelho();
+
+        concessionaria.setAno(Prompt.lerInteiro("Digite o ano do carro: "));
+
+        if(concessionaria.getAno() <= 2000){
+            ate2000++;
+        }
+            
+        total ++;
+
+        concessionaria.setPreco(Prompt.lerDecimal("Setar preço"));
+
+        
+
+
+        }
+
+    }
 
 }
