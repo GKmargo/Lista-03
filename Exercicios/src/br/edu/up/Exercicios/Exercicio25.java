71bc47bc74d100cad485ae8a0bd70586d577b131
Pedro Henrique Boldori Bier <124646793+Pedrohbbier@users.noreply.github.com>
2024-04-26 14:13:17 -0300

    Finalizando ex 25

--- Exercicios/src/br/edu/up/Exercicios/Exercicio25.java
+++ Exercicios/src/br/edu/up/Exercicios/Exercicio25.java
@@ -9,7 +9,29 @@ public class Exercicio25 {
 
         MediaPonderada MediaPonderada = new MediaPonderada();
 
-        
+        MediaPonderada.setNome(Prompt.lerLinha("Digite o nome: "));
+
+        String nome = MediaPonderada.getNome();
+
+        MediaPonderada.setMatricula(Prompt.lerInteiro("Digite o número de matricula do aluno " + nome));
+
+        int matricula = MediaPonderada.getMatricula();
+
+        MediaPonderada.setNota1(Prompt.lerDecimal("Digite a nota do Laboratorio: "));
+        MediaPonderada.setNota2(Prompt.lerDecimal("Digite a nota da avaliação semsteral:"));
+        MediaPonderada.setNota3(Prompt.lerDecimal("Digite a nota do Exame final:"));
+
+
+        Prompt.imprimir("\nNome: " + nome);
+        Prompt.imprimir("Matricula: " + matricula);
+        Prompt.imprimir("Nota final: " + MediaPonderada.mediaFinal());
+        Prompt.imprimir(MediaPonderada.classificacaoFinal());
+
+
+
+
+
+
 
     }
 
