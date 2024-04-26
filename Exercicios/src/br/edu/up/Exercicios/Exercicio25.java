6f54227ca923115477e6ebd90f6ce02da8ea526a
Pedro Henrique Boldori Bier <124646793+Pedrohbbier@users.noreply.github.com>
2024-04-26 14:04:28 -0300

    Iniciando ex25

---
+++ Exercicios/src/br/edu/up/Exercicios/Exercicio25.java
@@ -0,0 +1,16 @@
+package br.edu.up.Exercicios;
+
+import br.edu.up.Exercicios.Modelos.MediaPonderada;
+
+
+public class Exercicio25 {
+
+    public static void executar(){
+
+        MediaPonderada MediaPonderada = new MediaPonderada();
+
+        
+
+    }
+
+}
