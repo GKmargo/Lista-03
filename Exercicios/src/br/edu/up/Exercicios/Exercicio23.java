e3c56c3be22940ed33824b0d4e7b2d8bb1ee35f6
Pedro Henrique Boldori Bier <124646793+Pedrohbbier@users.noreply.github.com>
2024-04-26 14:02:47 -0300

    Peso Ideal

--- Exercicios/src/br/edu/up/Exercicios/Exercicio23.java
+++ Exercicios/src/br/edu/up/Exercicios/Exercicio23.java
@@ -5,7 +5,20 @@ import br.edu.up.Exercicios.Modelos.PesoIdeal;
 public class Exercicio23 {
 
     public static void executar(){
-        
+
+        PesoIdeal peso = new PesoIdeal();
+
+        peso.setNome(Prompt.lerLinha("Digite o seu nome: "));
+        peso.setAltura(Prompt.lerDecimal("Digite a sua altura: "));
+        peso.setIdade(Prompt.lerInteiro("Digite a sua idade"));
+        peso.setSexo(Prompt.lerCaractere("Digite o seu sexo, (M) (F)"));
+
+        String nome = peso.getNome();
+
+        Prompt.imprimir("O peso ideal de "+nome + " é " +  peso.calculo());
+
+
+
     }
 
 }
