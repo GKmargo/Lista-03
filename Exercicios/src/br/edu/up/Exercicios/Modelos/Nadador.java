e09caffa4b7c693319941ef358021337d89cafc1
Pedro Henrique Boldori Bier <124646793+Pedrohbbier@users.noreply.github.com>
2024-04-26 13:52:03 -0300

    Exercicio 21 Fim

--- Exercicios/src/br/edu/up/Exercicios/Modelos/Nadador.java
+++ Exercicios/src/br/edu/up/Exercicios/Modelos/Nadador.java
@@ -2,4 +2,17 @@ package br.edu.up.Exercicios.Modelos;
 
 public class Nadador {
 
+    private int idade;
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    
+
+
 }
