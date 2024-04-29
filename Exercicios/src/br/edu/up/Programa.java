020f2090c181a56ca0dffa8bdc8881a1667bbd09
Pedro Henrique Boldori Bier <124646793+Pedrohbbier@users.noreply.github.com>
2024-04-29 08:33:53 -0300

    Menu

--- Exercicios/src/br/edu/up/Programa.java
+++ Exercicios/src/br/edu/up/Programa.java
@@ -3,32 +3,119 @@ package br.edu.up;
 import br.edu.up.Exercicios.*;
 
 public class Programa {
-    public static void main(String[] args){
-        //Exercicio01.executar();
-        //Exercicio02.executar();
-        //Exercicio03.executar();
-        //Exercicio04.executar();
-        //Exercicio05.executar();
-        //Exercicio06.executar();
-        //Exercicio07.executar();
-        //Exercicio08.executar();
-        //Exercicio09.executar();
-        //Exercicio10.executar();
-        //Exercicio11.executar();
-        //Exercicio12.executar();
-        //Exercicio13.executar();
-        //Exercicio14.executar();
-        //Exercicio15.executar();
-        //Exercicio16.executar();
-        //Exercicio17.executar();
-        //Exercicio18.executar();
-        //Exercicio19.executar();
-        //Exercicio20.executar();
-        //Exercicio21.executar();
-        //Exercicio22.executar();
-        //Exercicio23.executar();
-        //Exercicio24.executar();
-        Exercicio25.executar();
-        //Exercicio26.executar();
+    public static void main(String[] args) {
+
+        int ex = Prompt.lerInteiro("Digite o exercício que deseja executar: ");
+
+        switch (ex) {
+            case 1:
+                Exercicio01.executar();
+                break;
+
+            case 2:
+                Exercicio02.executar();
+                break;
+
+            case 3:
+                Exercicio03.executar();
+                break;
+
+            case 4:
+                Exercicio04.executar();
+                break;
+
+            case 5:
+                Exercicio05.executar();
+                break;
+
+            case 6:
+                Exercicio06.executar();
+                break;
+
+            case 7:
+                Exercicio07.executar();
+                break;
+
+            case 8:
+                Exercicio08.executar();
+                break;
+
+            case 9:
+                Exercicio09.executar();
+                break;
+
+            case 10:
+                Exercicio10.executar();
+                break;
+
+            case 11:
+                Exercicio11.executar();
+                break;
+
+            case 12:
+                Exercicio12.executar();
+                break;
+
+            case 13:
+                Exercicio13.executar();
+                break;
+
+            case 14:
+                Exercicio14.executar();
+                break;
+
+            case 15:
+                Exercicio15.executar();
+                break;
+
+            case 16:
+                Exercicio16.executar();
+                break;
+
+            case 17:
+                Exercicio17.executar();
+                break;
+
+            case 18:
+                Exercicio18.executar();
+                break;
+
+            case 19:
+                Exercicio19.executar();
+                break;
+
+            case 20:
+                Exercicio20.executar();
+                break;
+
+            case 21:
+                Exercicio21.executar();
+                break;
+
+            case 22:
+                Exercicio22.executar();
+                break;
+
+            case 23:
+                Exercicio23.executar();
+                break;
+
+            case 24:
+                Exercicio24.executar();
+                break;
+
+            case 25:
+                Exercicio25.executar();
+                break;
+
+            case 26:
+                Exercicio26.executar();
+                break;
+
+            default:
+                Prompt.imprimir("Exercício não encontrado!");
+                break;
+        }
+
     }
 }
